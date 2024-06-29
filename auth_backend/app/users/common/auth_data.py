a1523ddef969fa7d5be3fc8deb28e3fffe81cd55
OlehDziuba <oleg_dzuba@outlook.com>
2024-06-29 16:57:04 +0300

    Added normalization for 'AuthData.email' field

--- auth_backend/app/users/common/auth_data.py
+++ auth_backend/app/users/common/auth_data.py
@@ -9,3 +9,7 @@ class AuthData(pydantic.BaseModel):
 
     def hash(self, hasher: PasswordHasher) -> None:
         self.password = hasher.hash(self.password)
+
+    @pydantic.field_validator("email")
+    def _normalize_email(self, email: str) -> str:
+        return email.lower().strip()
