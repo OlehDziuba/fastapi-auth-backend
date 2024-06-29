b60ab52f4006deb12316a9e770a66c541e19c4f2
OlehDziuba <oleg_dzuba@outlook.com>
2024-06-29 16:56:43 +0300

    Fix 'AuthData.password' field typing

--- auth_backend/app/users/common/auth_data.py
+++ auth_backend/app/users/common/auth_data.py
@@ -5,7 +5,7 @@ from auth_backend.app.users.abc import PasswordHasher
 
 class AuthData(pydantic.BaseModel):
     email: pydantic.EmailStr
-    password: pydantic.EmailStr
+    password: str
 
     def hash(self, hasher: PasswordHasher) -> None:
         self.password = hasher.hash(self.password)
