3c69bf55739bce63804378a3f4c45c858b305ab2
OlehDziuba <oleg_dzuba@outlook.com>
2024-06-29 16:52:58 +0300

    Added handling errors in 'get_me' endpoint

--- auth_backend/api/users/get_me.py
+++ auth_backend/api/users/get_me.py
@@ -1,9 +1,9 @@
 from typing import Annotated
 
 from dependency_injector.wiring import Provide, inject
-from fastapi import Depends, Header
+from fastapi import Depends, Header, HTTPException, status
 
-from auth_backend.app import GetUserByTokenInteractor
+from auth_backend.app import GetUserByTokenInteractor, InvalidTokenError, UserEmailDoesNotExistError
 from .common import UserResponse
 
 
@@ -12,6 +12,9 @@ async def get_me(
         authorization: Annotated[str, Header()],
         interactor: GetUserByTokenInteractor = Depends(Provide["get_user_by_token_interactor"])
 ) -> UserResponse:
-    user = await interactor(authorization)
+    try:
+        user = await interactor(authorization)
+    except (InvalidTokenError, UserEmailDoesNotExistError):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
     return UserResponse.from_user(user)
